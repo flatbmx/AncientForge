8bf175853ee820acfc2156c05387edf7d14ba0aa
flatbmx <lurtz1122334@hotmail.com>
2012-11-25 15:17:36 -0600

    added list of prefixs/suffixs, also added method for gettting random prefix/suffix.

--- src/com/podts/ancientforge/namemodifier/ItemPrefix.java
+++ src/com/podts/ancientforge/namemodifier/ItemPrefix.java
@@ -1,15 +1,40 @@
 package com.podts.ancientforge.namemodifier;
 
+import java.util.HashMap;
+import java.util.Random;
+
 import org.bukkit.ChatColor;
 
 public class ItemPrefix extends NameModifier {
 	
+	private static HashMap<String, ItemPrefix> prefixs = new HashMap<String, ItemPrefix>();
+	
+	public static HashMap<String, ItemPrefix> getPrefixs() {
+		return prefixs;
+	}
+	
+	public static ItemPrefix getRandomPrefix() {
+		Random r = new Random();
+		return (ItemPrefix) prefixs.values().toArray()[ r.nextInt( r.nextInt( prefixs.values().size() ) ) ];
+	}
+	
+	private static void addPrefix(ItemPrefix prefix) {
+		
+		if ( prefixs.containsKey(prefix.getName()) )
+			return;
+		
+		prefixs.put(prefix.getName(), prefix);
+		
+	}
+	
 	public ItemPrefix(String name) {
 		super(name);
+		addPrefix(this);
 	}
 	
 	public ItemPrefix(String name, ChatColor color) {
 		super(name, color);
+		addPrefix(this);
 	}
 	
 }
--- src/com/podts/ancientforge/namemodifier/ItemSuffix.java
+++ src/com/podts/ancientforge/namemodifier/ItemSuffix.java
@@ -1,15 +1,40 @@
 package com.podts.ancientforge.namemodifier;
 
+import java.util.HashMap;
+import java.util.Random;
+
 import org.bukkit.ChatColor;
 
 public class ItemSuffix extends NameModifier{
 	
+private static HashMap<String, ItemSuffix> suffixs = new HashMap<String, ItemSuffix>();
+	
+	public static HashMap<String, ItemSuffix> getSuffixs() {
+		return suffixs;
+	}
+	
+	public static ItemSuffix getRandomPrefix() {
+		Random r = new Random();
+		return (ItemSuffix) suffixs.values().toArray()[ r.nextInt( r.nextInt( suffixs.values().size() ) ) ];
+	}
+	
+	private static void addSuffix(ItemSuffix suffix) {
+		
+		if ( suffixs.containsKey(suffix.getName()) )
+			return;
+		
+		suffixs.put(suffix.getName(), suffix);
+		
+	}
+	
 	public ItemSuffix(String name) {
 		super(name);
+		addSuffix(this);
 	}
 	
 	public ItemSuffix(String name, ChatColor color) {
 		super(name, color);
+		addSuffix(this);
 	}
 	
 }
