45b79f617f119ecaf6d6b8253f7d6115d230908b
Will2233 <klusmannwilliam@yahoo.com>
2012-11-25 00:04:01 -0600

    fixed .gitigore

---
+++ src/com/podts/ancientforge/suffix/Suffix_Test.java
@@ -0,0 +1,12 @@
+package com.podts.ancientforge.suffix;
+
+import com.podts.ancientforge.namemodifier.ItemSuffix;
+
+public class Suffix_Test extends ItemSuffix {
+
+	public Suffix_Test() {
+		super("Test");
+		getEffects().setDamgeincrease(3);
+	}
+
+}
