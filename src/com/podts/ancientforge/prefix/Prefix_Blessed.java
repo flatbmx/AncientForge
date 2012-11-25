4e9be2ae79a4ddaf48e2467e874c45091a9e1be8
flatbmx <lurtz1122334@hotmail.com>
2012-11-25 00:00:29 -0600

    fixed .gitignore

---
+++ src/com/podts/ancientforge/prefix/Prefix_Blessed.java
@@ -0,0 +1,12 @@
+package com.podts.ancientforge.prefix;
+
+import com.podts.ancientforge.namemodifier.ItemPrefix;
+
+public class Prefix_Blessed extends ItemPrefix {
+
+	public Prefix_Blessed() {
+		super("Blessed");
+		getEffects().setArmorincrease(1);
+	}
+
+}
