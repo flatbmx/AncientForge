25a583f5e35471049c9804548e699baf8b72cb86
flatbmx <lurtz1122334@hotmail.com>
2012-11-27 20:20:36 -0600

    Updated to 0.06 aswell as anvil/enchantment table protections from item manipulation.

--- src/com/podts/ancientforge/listeners/InventoryHandler.java
+++ src/com/podts/ancientforge/listeners/InventoryHandler.java
@@ -1,10 +1,16 @@
 package com.podts.ancientforge.listeners;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.craftbukkit.inventory.CraftItemStack;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.enchantment.PrepareItemEnchantEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.AnvilInventory;
 import org.bukkit.inventory.ItemStack;
 
 import com.podts.ancientforge.MagicItem;
@@ -12,6 +18,40 @@ import com.podts.ancientforge.NamedItem;
 import com.podts.ancientforge.player.AFPlayer;
 
 public class InventoryHandler implements Listener {
+	
+	@EventHandler
+	public void OnEnchantInsertion(PrepareItemEnchantEvent event) {
+		
+		if (NamedItem.isPluginItem((CraftItemStack) event.getItem())) {
+			
+			event.setCancelled(true);
+			
+		}
+		
+	}
+	
+	@EventHandler
+	public void OnInventoryClickEvent(InventoryClickEvent event) {
+		
+		if ( !(event.getInventory().getType().equals(InventoryType.ANVIL) ) )
+			return;
+		
+		if ( !(event.getInventory() instanceof AnvilInventory) )
+			return;
+		
+		if (event.getSlot() != 9)
+			return;
+		
+		if (event.getCurrentItem().getType().equals(Material.AIR))
+			return;
+		
+		if (NamedItem.isPluginItem((CraftItemStack) event.getCurrentItem())) {
+			event.setCancelled(true);
+			Player p = (Player) event.getWhoClicked();
+			p.sendMessage(ChatColor.DARK_RED + "You cannot repair this item.");
+		}
+		
+	}
 	
 	@EventHandler
 	public void OnInventoryCloseEvent(InventoryCloseEvent event) {
