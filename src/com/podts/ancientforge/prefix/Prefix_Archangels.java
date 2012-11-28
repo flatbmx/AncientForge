0205d2a87127ce54ab5f981f8b630d94e8c111df
Will2233 <klusmannwilliam@yahoo.com>
2012-11-27 23:06:42 -0600

    Added New Suffix/Prefix

---
+++ src/com/podts/ancientforge/prefix/Prefix_Archangels.java
@@ -0,0 +1,15 @@
+package com.podts.ancientforge.prefix;
+
+import org.bukkit.ChatColor;
+
+import com.podts.ancientforge.namemodifier.ItemPrefix;
+
+public class Prefix_Archangels extends ItemPrefix {
+	
+	public Prefix_Archangels() {
+		super("Archangel's", ChatColor.DARK_RED);
+		getEffects().setDamgeincrease(3);
+		
+	}
+
+}
---
+++ src/com/podts/ancientforge/suffix/Suffix_OfTheFlames.java
@@ -0,0 +1,14 @@
+package com.podts.ancientforge.suffix;
+
+import org.bukkit.ChatColor;
+
+import com.podts.ancientforge.namemodifier.ItemSuffix;
+
+public class Suffix_OfTheFlames extends ItemSuffix {
+
+	public Suffix_OfTheFlames() {
+		super("Of The Flames", ChatColor.DARK_RED);
+		getEffects().setDamgeincrease(3);
+	}
+
+}
