1ceb6971d1d7ab42c9b620034481350e24d1b1ac
Will2233 <klusmannwilliam@yahoo.com>
2012-11-27 23:20:36 -0600

    Fixed.settings

--- src/com/podts/ancientforge/suffix/Suffix_OfTheFortunate.java
+++ src/com/podts/ancientforge/suffix/Suffix_OfTheFortunate.java
@@ -7,7 +7,7 @@ import com.podts.ancientforge.namemodifier.ItemSuffix;
 public class Suffix_OfTheFortunate extends ItemSuffix {
 
 	public Suffix_OfTheFortunate() {
-		super("Of The Forutunate", ChatColor.GOLD);
+		super("Of The Fortunate", ChatColor.GOLD);
 		getEffects().setFindmagicitems(.03f);
 	}
 
